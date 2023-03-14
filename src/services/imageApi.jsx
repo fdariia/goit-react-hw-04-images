ccff3c2f7447e4152b3c6602d78817256f3783a3
fdariia <fdariya3005@gmail.com>
2023-03-14 02:47:39 +0200

    Update imageApi.jsx

--- src/services/imageApi.jsx
+++ src/services/imageApi.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://pixabay.com/api';
 const API_KEY = '34173138-558a9ee15c3093426fbf117d9';
 
-export const getImage = async (page, category) => {
+export const getImage = async (category, page) => {
   return await axios.get(
     `/?q=${category}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
   );
